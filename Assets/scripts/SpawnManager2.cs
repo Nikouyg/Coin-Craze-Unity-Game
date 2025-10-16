b7c4150e508da3968edb6ad9bbab28042866e4d7
Nikou <nikooyousefzadeh@gmail.com>
2025-10-17 01:26:24 +0300

    made new obstacle to spawn on a timer

--- Assets/scripts/SpawnManager2.cs
+++ Assets/scripts/SpawnManager2.cs
@@ -3,16 +3,27 @@ using UnityEngine;
 public class SpawnManager2 : MonoBehaviour
 {
      public GameObject obstaclePrefab;
-    private Vector3 spawnPos= new Vector3(33,5,0);
+    private Vector3 spawnPos = new Vector3(20, 5, 0);
+     private float startDelay=4;
+    private float repeatRate=3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-         Instantiate(obstaclePrefab,spawnPos,obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    void SpawnObstacle()
+    {
         
+            Instantiate(obstaclePrefab,spawnPos,obstaclePrefab.transform.rotation);
+
+
+
     }
 }
